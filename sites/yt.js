b229ecdfa63b7e57d98f84f69c320734a7b1d9e6
Max <silverhammermba@gmail.com>
2013-09-30 18:25:50 -0400

    Hide dynamic elements on Youtube
    Occurs when visiting related videos

--- sites/yt.js
+++ sites/yt.js
@@ -1,7 +1,9 @@
-// comments
-hide_id('watch-discussion');
-// like/dislike buttons
-hide_id('watch7-sentiment-actions');
-// like/dislike display
-hide_class('video-extras-sparkbars');
-hide_class('video-extras-likes-dislikes');
+always(function() {
+	// comments
+	hide_id('watch-discussion');
+	// like/dislike buttons
+	hide_id('watch7-sentiment-actions');
+	// like/dislike display
+	hide_class('video-extras-sparkbars');
+	hide_class('video-extras-likes-dislikes');
+});
